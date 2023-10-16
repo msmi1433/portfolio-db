e51abc1980ec1b7f5ecae5b03a640c9eb5f52718
Myles Smith <mylessmith@Myless-MacBook-Pro.local>
2023-10-16 12:07:35 +0100

    tweaks project descriptions

--- db/data/test_data/project.js
+++ db/data/test_data/project.js
@@ -2,7 +2,7 @@ module.exports = [
   {
     name: "MSMI News",
     description:
-      "This project provides the user with an interactive news website, utilising an API created with Express.js and a frontend built with React.\nThe app allows users to read various news articles, with filtering and sort options available. Users can also interact with articles by voting on them and posting comments. Comments have a votes feature and can be deleted when logged in as the user that posted the comment (user selection is implemented without authentication, currently).",
+      "This project provides the user with an interactive news website, utilising an API created with Express.js and a frontend built with React.\nThe app allows users to read various news articles, with filtering and sorting options available. Users can also interact with articles by voting on them and posting comments. Comments have a votes feature and can be deleted when logged in as the user that posted the comment (user selection is implemented without authentication, currently).",
     project_link: "https://msmi-news.netlify.app/",
     githubLinkFe: "https://github.com/msmi1433/msmi-news",
     githubLinkBe: "https://github.com/msmi1433/nc-news-project",
@@ -12,7 +12,7 @@ module.exports = [
   {
     name: "Shelf Indulgence",
     description:
-      "Shelf Indulgence is an app-based solution to book clubs, providing users with a digital space to connect with like-minded readers and share their love of literature.\nThe app allows users to create and join book clubs which house a number of features, aiming to replicate the 'real-world' book club experience.",
+      "Shelf Indulgence is an app-based solution to book clubs, providing users with a digital space to connect with like-minded readers and share their love of literature.\nThe app allows users to create and join book clubs which house a number of features, aiming to replicate the 'real-world' book club experience. Shelf Indulgence was built during the final group project phase of the Northocders bootcamp, and I am planning to refine/add new features to the app moving forward.",
     project_link: null,
     githubLinkFe: "https://github.com/msmi1433/Bookclub",
     githubLinkBe: null,
